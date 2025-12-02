6a04fbb9e986680d8705329bfa3ed5ec9a52fa95
bugparty <fancycode@gmail.com>
2025-12-02 04:58:29 +0000

    fix: update permissions and ensure output directory creation in dummy_reward.sh

--- examples/dummy_reward.sh
+++ examples/dummy_reward.sh
@@ -8,6 +8,6 @@ if ! nm -gU "$file" | grep -q "$symbol"; then
     echo "symbol not found"
     exit 1
 fi
-
+mkdir -p out
 export TAINT_OPTIONS="output_dir=out:taint_file=stdin:debug=1"
 ../build/bin/fgtest ./dummy seed.bin ctwm_index.json dummy_traces.json rewards.json
